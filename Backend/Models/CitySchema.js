226c9d1dd8ba4d595c08ab42d0739f7cbaf536d1
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-03-18 16:31:25 +0530

    Created Schemas for Cities and States with AI-review changes

--- Backend/Models/CitySchema.js
+++ Backend/Models/CitySchema.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const citySchema = mongoose.Schema({
     Name: {type: String,required: [true, "Please add the Name of city"]},
     State_Id: {type: String},
-    Latitude: {type: String},
+    Latitude: {type: Number},
     Longitude: {type: Number},
-    Location: {type: Number},
+    Location: {type: String},
     Image: {type: String,required: [true, "Please add image link of the city"]},
     Description: {type: String,required: [true, "Please add the description about the city"]},
 })
