0ecf71dfcd19810e51d7baab65ae46d0749e0c4f
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-03-30 17:42:14 +0530

    Footer with responsiveness

--- RoamIndia/src/Components/Footer.tsx
+++ RoamIndia/src/Components/Footer.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import Phone from './../assets/Phone.png'
 import Email from './../assets/Email.png'
 import Location from './../assets/Location.png'
+import Logo from './../assets/RoamIndiaBigLogo.png'
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      <div className="bg-[#133039] flex  justify-between p-7 items-center text-white">
+    <div className="bg-[#133039] text-white px-20">
+      <div className=" flex  justify-between p-7 items-center">
         <div className="flex gap-3">
           <div className="border-r-[0.2px] pr-3 asul-regular text-lg">ROAM INDIA</div>
           <div className="text-sm">DISCOVER
@@ -29,7 +30,59 @@ const Footer: React.FC = () => {
           </div>
         </div>
       </div>
-      <hr className="w-7/12"/>
+      <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-8" />
+      <div className="flex">
+        <div>
+          <img src={Logo} alt="" className="w-6/12 ml-20" />
+        </div>
+        <form className="w-4/12 flex flex-col gap-10">
+          <div className="relative z-0 w-full">
+            <input
+              type="text"
+              id="default_standard"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#4BD4FF] focus:outline-none focus:ring-0 focus:border-[#4BD4FF] peer "
+              placeholder=""
+            />
+            <label
+              htmlFor="default_standard"
+              className="absolute text-xl  text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#4BD4FF] peer-focus:dark:text-[#4BD4FF]peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+            >
+              Name (Required)
+            </label>
+          </div>
+          <div className="relative z-0 w-full">
+            <input
+              type="text"
+              id="default_standard"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#4BD4FF] focus:outline-none focus:ring-0 focus:border-[#4BD4FF] peer "
+              placeholder=""
+            />
+            <label
+              htmlFor="default_standard"
+              className="absolute text-xl  text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#4BD4FF] peer-focus:dark:text-[#4BD4FF]peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+            >
+              Email (Required)
+            </label>
+          </div>
+          <div className="relative z-0 w-full">
+            <input
+              type="text"
+              id="default_standard"
+              className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#4BD4FF] focus:outline-none focus:ring-0 focus:border-[#4BD4FF] peer "
+              placeholder=""
+            />
+            <label
+              htmlFor="default_standard"
+              className="absolute text-xl  text-white dark:text-white duration-300 transform -translate-y-6 scale-75 top-0 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-[#4BD4FF] peer-focus:dark:text-[#4BD4FF]peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto"
+            >
+              How can we help you?  (Required)
+            </label>
+          </div>
+          <button className="btn text-white bg-black w-40">Submit</button>
+        </form>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 RoamIndia. All Rights Reserved. | Designed by Megha Wadhwa</span>
     </div>
   );
 };
