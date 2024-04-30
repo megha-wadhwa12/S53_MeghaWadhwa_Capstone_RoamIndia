ef2c379207db8612f5fd63f683e7179edb716570
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-04-30 18:12:18 +0530

    Removed Password

--- Backend/Models/UserSchema.js
+++ Backend/Models/UserSchema.js
@@ -4,7 +4,6 @@ const userSchema = mongoose.Schema({
     "Name": {type: String,required: [true, "Please Enter Your Name"]},
     "userName": {type: String,required: [true, "Please Enter Your Username"]},
     "emailId": {type: String,required: [true, "Please Add Your e-mail address"]},
-    "Password": {type: String,required: [true, "Please Enter Your Password"]},
     "Favourites": [String]
 })
 
