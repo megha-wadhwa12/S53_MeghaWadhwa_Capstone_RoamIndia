eb65e918f9c2c5e79fabe45d4d80f7b7c00af576
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-05-01 16:19:42 +0530

    Schema Updated

--- Backend/Models/StateSchema.js
+++ Backend/Models/StateSchema.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const stateSchema = mongoose.Schema({
     State_Name: {type: String,required: [true, "Please add the Name of State"]},
+    State_Code: {type: String,required: [true,"Please add state code"]},
     Popular_Attractions: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'attractionschemas'
     }],
-    State_Code: {type: String,required: [true,"Please add state code"]},
     Image: {type: String,required: [true, "Please add image link of the State"]},
     Description: {type: String,required: [true, "Please add the description about the state"]}
     
